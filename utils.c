8cef5112aeaddafe224f5cd76e8ff39ff16b2f4e
Salil <slashforu@gmail.com>
2012-04-06 13:18:42 -0400

    GShare Branch Predictor Simulator Working.
    Next Up - Hybrid Branch Predictor Simulator.
    
    Signed-off-by: Salil <slashforu@gmail.com>

--- utils.c
+++ utils.c
@@ -15,7 +15,7 @@ void initialize_pred_params(predictor_t *predictor)
 		predictor->config.K = 0;
 	} else if (strcmp(sim_type, "gshare") == 0) {
 		strcpy(predictor->config.sim_type, sim_type);
-		predictor->config.M2 = M2;
+		predictor->config.M2 = 0;
 		predictor->config.M1 = M1;
 		predictor->config.N = N;
 		predictor->config.K = 0;
@@ -49,6 +49,27 @@ void allocate_and_init_pred_tab(predictor_t *predictor)
 		for (i=0 ; i < predictor->pred_table.size_in_bytes ; i++) {
 			predictor->pred_table.table[i] = 0xAA;
 		}
+	} else if (strcmp(predictor->config.sim_type, "gshare") == 0) {
+
+		predictor->config.num_entries = pow(2, predictor->config.M1);
+		predictor->config.bits_per_entry = COUNTER_BITS;
+		predictor->pred_table.size_in_bytes = predictor->config.num_entries * predictor->config.bits_per_entry / BITS_PER_BYTE;
+		predictor->config.entries_per_byte = BITS_PER_BYTE / predictor->config.bits_per_entry;
+
+		predictor->pred_table.table = (unsigned char *)malloc(sizeof(unsigned char) * predictor->pred_table.size_in_bytes);
+		if (predictor->pred_table.table == NULL) {
+			printf("Error while allocating Memory!!\n");
+			printf("Exiting...\n");
+			exit(1);
+		}
+
+		/* Initialize all the counters in Prediction Table to 2 (weakly taken) */
+		for (i=0 ; i < predictor->pred_table.size_in_bytes ; i++) {
+			predictor->pred_table.table[i] = 0xAA;
+		}
+
+		/* Initialize Branch History Register to all Zeros*/
+		predictor->bhr = 0;
 	}
 }
 
@@ -72,6 +93,7 @@ void handle_branch_prediction(predictor_t *predictor, pc_t *pc)
 {
 	int index;
 	int base, offset, byte, count;
+	int m_pc_bits, n_pc_bits, lower_pc_bits, msb_bhr;
 	int this_prediction, is_misprediction=0;
 
 	if (strcmp(predictor->config.sim_type, "bimodal") == 0) {
@@ -119,5 +141,86 @@ void handle_branch_prediction(predictor_t *predictor, pc_t *pc)
 		printf("\tBU: %d %d\n", index, count);
 #endif
 
+	} else if (strcmp(predictor->config.sim_type, "gshare") == 0) {
+
+#ifdef DEBUG_OP
+		printf("=%ld\t%x %c\n", predictor->config.num_predictions, pc->addr, pc->branch_outcome);
+#endif
+
+		/* Extracting index:
+			1) First we extract 2 to M1+1 bits of pc - number of bits extracted will be M1.
+			2) Then from these M1 bits, we extract upper N bits.
+			3) XOR these upper N bits with N-bits long BHR (Branch History Register).
+			4) Append the lower bits (n-m) of pc extracted bits to result of XOR - forming the index.
+		*/
+		m_pc_bits = extract_bits(pc->addr, 2, predictor->config.M1 + 1);
+		lower_pc_bits = extract_bits(m_pc_bits, 0, predictor->config.M1 - predictor->config.N - 1);
+		n_pc_bits = extract_bits(m_pc_bits, predictor->config.M1 - predictor->config.N, predictor->config.M1 - 1);
+
+		index = n_pc_bits ^ predictor->bhr;
+		index = index << (predictor->config.M1 - predictor->config.N);
+		index = index ^ lower_pc_bits;
+
+		/* The index will provide a unique prediction counter.
+		   However, since each counter is a 2 bit entity, index is first split into base and offset.
+		   Base points to the byte which contains the required indexed count. Offset points to the first bit of the two-bit
+		   counter within the byte indexed by the base.
+		*/
+		base = index / predictor->config.entries_per_byte;
+		offset = index % predictor->config.entries_per_byte;
+		offset = offset * 2;
+		byte = predictor->pred_table.table[base];
+		count = extract_bits(byte, offset, offset + predictor->config.bits_per_entry - 1);
+
+#ifdef DEBUG_OP
+		printf("\tGP: %d %d\n", index, count);
+#endif
+
+		if (count == 0 || count == 1) {
+			this_prediction = NOT_TAKEN;
+		} else if (count == 2 || count == 3) {
+			this_prediction = TAKEN;
+		}
+
+		is_misprediction = 0;
+		if (pc->branch_outcome == 'n' && this_prediction == TAKEN) {
+			is_misprediction = 1;
+			predictor->config.num_mispredictions += 1;
+		} else if (pc->branch_outcome == 't' && this_prediction == NOT_TAKEN) {
+			is_misprediction = 1;
+			predictor->config.num_mispredictions += 1;
+		}
+
+		if (pc->branch_outcome == 't') {
+			if (count < 3)
+				count += 1;
+		} else if (pc->branch_outcome == 'n') {
+			if (count > 0)
+				count -= 1;
+		}
+
+		predictor->pred_table.table[base] = update_bits(byte, offset, offset + predictor->config.bits_per_entry - 1, count);
+
+		msb_bhr = 0;
+		if (pc->branch_outcome == 'n')
+			msb_bhr = 0;
+		else if (pc->branch_outcome == 't')
+			msb_bhr = 1;
+
+		/* Updating BHR (Branch History Register)
+		   BHR is shifted Right by one bit.
+		   The actual outcome of the branch is then put as the Most Significant Bit of the BHR (i.e. N-1th bit of BHR).
+		   This updated BHR will then be used in the Prediction stage while XORing with PC bits. (See Above).
+		*/
+		predictor->bhr = predictor->bhr >> 1;
+		msb_bhr = msb_bhr << (predictor->config.N - 1);
+		predictor->bhr = predictor->bhr ^ msb_bhr;
+
+#ifdef DEBUG_OP
+		printf("\tGU: %d %d\n", index, count);
+#endif
+
 	}
 }
+
+
